fc8c88505c1b06b7233dccb39b979ae66cfca057
neil001002 <neil001002@gmail.com>
2024-06-21 02:09:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/form";
+
+function App() {
+  return (
+    <section className="py-24 ">
+      <div className="container ">
+        <Form />
+      </div>
+    </section>
+  );
+}
+
+export default App;
